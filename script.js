9fc232a0f519ef8a7a3fcccd74b4d738ada3089d
wouterbus <59446222+wouterbus@users.noreply.github.com>
2024-02-19 19:28:44 -0300

    Uploading FIles

---
+++ script.js
@@ -0,0 +1,11 @@
+const checkbox = document.getElementById('chk');
+
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+    }
+});
